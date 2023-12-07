ba3f096546f410d9ea0a25912348e98ed706334c
BuildTools <unconfigured@null.spigotmc.org>
2023-12-07 21:59:11 +0900

    SpringBoot Practice v3 DataBase Setup

---
+++ src/main/resources/SQL/webapp_contact_data.sql
@@ -0,0 +1,11 @@
+use webapp;
+set names sjis;
+insert into contact(
+    order_number,
+    name,
+    email,
+    title,
+    contents,
+    recv_date
+) values
+("20231024001","名前","toramaru@show","タイトル","内容","2023-10-23 00:00:00");
